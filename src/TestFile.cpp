43ceac7fbdba133cb2b4416fcbd54a079575e567
Guillem Sunyer <gsunyercaldu@gmail.com>
2024-01-31 12:13:26 +0100

    Initial cmake

---
+++ src/TestFile.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Guillem on 31/01/2024.
+//
+
+#include "TestFile.h"
---
+++ src/TestFile.h
@@ -0,0 +1,14 @@
+//
+// Created by Guillem on 31/01/2024.
+//
+
+#ifndef GENGINECPPGAME_TESTFILE_H
+#define GENGINECPPGAME_TESTFILE_H
+
+
+class TestFile {
+
+};
+
+
+#endif //GENGINECPPGAME_TESTFILE_H
