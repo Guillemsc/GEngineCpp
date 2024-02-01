7d3240b931a5f11004124f46a76f857138999715
Guillem Sunyer <gsunyercaldu@gmail.com>
2024-02-02 00:19:13 +0100

    Compile error?

--- src/Contexts/Renderer3D/Modules/Renderer3DModule.cpp
+++ src/Contexts/Renderer3D/Modules/Renderer3DModule.cpp
@@ -38,9 +38,9 @@ namespace GEngine
     void Renderer3DModule::Render()
     {
         Camera3D camera = { 0 };
-        camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
-        camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
-        camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+        camera.position = { 10.0f, 10.0f, 10.0f }; // Camera position
+        camera.target = { 0.0f, 0.0f, 0.0f };      // Camera looking at point
+        camera.up = { 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
         camera.fovy = 45.0f;                                // Camera field-of-view Y
         camera.projection = CAMERA_PERSPECTIVE;
 
